f13276ac5ebf8f0679c8512aeee75875f018f4c6
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-03-13 12:56:42 +0000

    First commit with collision detection rays

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Set requiredComponents
+[RequireComponent (typeof (Controller2D))]
+
+public class Player : MonoBehaviour {
+
+    // 2d controller
+    Controller2D controller;
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<Controller2D>();
+	}
+	
+}
