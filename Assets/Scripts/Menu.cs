04dbef3bc3ff8dde557bbdc2998a3f61b7be6e58
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-03-16 18:03:08 +0000

    Implemented basic play, back and quit buttons to the main menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class Menu : MonoBehaviour {
+
+	// Function for navigating our main menu
+    public void PlayGame ()
+    {
+        // Increment our scene forward 1 scene from the scene we are on 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Function for quitting the game
+    public void QuitGame()
+    {
+        // Quit the game
+        Application.Quit();
+    }
+}
