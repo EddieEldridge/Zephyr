023d579536211a6786ce4d8b8b815ebf0713dd18
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-03-23 19:42:26 +0000

    Implementing player movement when on a moving platform

--- Assets/Scripts/PlatformController.cs
+++ Assets/Scripts/PlatformController.cs
@@ -15,8 +15,22 @@ public class PlatformController : RaycastController {
 	// Update is called once per frame
 	void Update () {
 
+        // Smoothly moves around our platform when we change its x and y values in the Unity inspector
         Vector3 velocity = move * Time.deltaTime;
         transform.Translate(velocity);
+    }
 
+    // Function to control movement of 'passengers' (i.e players standing on the moving platform)
+    void MovePassengers(Vector3 velocity)
+    {
+        // Variables
+        float directionX = Mathf.Sign(velocity.x);
+        float directionY = Mathf.Sign(velocity.y);
+
+        // Vertically moving platform
+        if(velocity.y !=0)
+        {
+
+        }
     }
 }
