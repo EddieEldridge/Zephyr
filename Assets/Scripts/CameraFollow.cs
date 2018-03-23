9b0cda81827aeb342eac6cc6963d901afd689544
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-03-23 23:16:01 +0000

    Added a new script that makes the camera follow our player

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    // Function to move our camera with our target 
+    void LateUpdate()
+    {
+        // Set our camera to focus on our target (the player)
+        transform.position = target.position + offset;
+    }
+}
