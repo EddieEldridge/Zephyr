7b6109268ee29b906b58a37e831824de8ebe71a2
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-03-23 20:15:16 +0000

    Added horizontally moving platforms

--- Assets/Scripts/PlatformController.cs
+++ Assets/Scripts/PlatformController.cs
@@ -81,6 +81,50 @@ public class PlatformController : RaycastController
 
             }
         }
+
+        // Horizontally moving platform
+        if (velocity.x != 0)
+        {
+            float rayLength = Mathf.Abs(velocity.x) + skinWidth;
+
+            // For loop to draw our vertical rays
+            for (int i = 0; i < horizontalRayCount; i++)
+            {
+                // If moving down, set raycastOrigins to bottomLeft
+                // otherwise, set raycastOrigins to topLeft
+                Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
+                rayOrigin += Vector2.up * (horizontalRaySpacing * i);
+
+
+                // Perform a raycast from our rayOrigin to the dire
+                RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, passengerMask);
+
+                // Draw our collision detection rays so we can see them for debugging
+                Debug.DrawRay(rayOrigin, Vector2.right * directionX * rayLength, Color.red);
+
+                // If hit (i.e there's a passenger on the platform)
+                if (hit)
+                {
+                    // If the passenger isn't in the hashSet of players that have already mvoed this frame
+                    if (!movedPassengers.Contains(hit.transform))
+                    {
+                        // Add the passenger to the hashSet 'movedPassengers'
+                        movedPassengers.Add(hit.transform);
+
+                        // Variables to move our passenger with our platform
+                        float pushY = 0;
+                        float pushX = velocity.x - (hit.distance - skinWidth) * directionX;
+
+                        // Move the player
+                        hit.transform.Translate(new Vector3(pushX, pushY));
+
+                    }
+
+                }
+            }
+        }
+
+
     }
 
 }
