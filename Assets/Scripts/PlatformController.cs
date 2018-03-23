a613f93fe7728a383703d8ea7b5e77fa471960c9
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-03-23 19:37:45 +0000

    Added a moving platform and attached our PlatformController

--- Assets/Scripts/PlatformController.cs
+++ Assets/Scripts/PlatformController.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlatformController : MonoBehaviour {
+public class PlatformController : RaycastController {
+
+    // Vector to move our platform
+    public Vector3 move;
 
 	// Use this for initialization
-	void Start () {
-		
+	public override void Start () {
+        base.Start();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        Vector3 velocity = move * Time.deltaTime;
+        transform.Translate(velocity);
+
+    }
 }
