c730fae25871fec2176e4e517ab0cb9dc5746079
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-03-24 00:12:58 +0000

    Reworked camera to follow the player a bit more smoothly

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,13 +5,12 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour {
 
     public Transform target;
-    public float smoothSpeed = 0.125f;
-    public Vector3 offset;
+
+    public float smoothSpeed = 5;
 
     // Function to move our camera with our target 
     void LateUpdate()
     {
-        // Set our camera to focus on our target (the player)
-        transform.position = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, new Vector3(target.position.x, target.position.y, transform.position.z), Time.deltaTime*smoothSpeed);
     }
 }
