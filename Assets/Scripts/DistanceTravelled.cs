3797418752051b707c6aa9e9d49266e17d96f91b
EddieEldridge <steadyeddie101@hotmail.co.uk>
2018-03-28 21:00:17 +0100

    Trying to implement a high score counter

--- Assets/Scripts/DistanceTravelled.cs
+++ Assets/Scripts/DistanceTravelled.cs
@@ -1,16 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI.Text;
 
-public class DistanceTravelled : MonoBehaviour {
+public class DistanceTravelled : MonoBehaviour 
+{
 
-	// Use this for initialization
-	void Start () {
-		
+	// Variables
+	private Vector3 startPosition;
+
+	// Setters and Getters
+	public int score 
+	{
+        get;
+        private set;
+    }
+ 
+    public int bestScore
+	{
+        get;
+        private set;
+    }
+   	
+	// Functions
+    void Awake()
+	 {
+        startPosition = transform.position;
+        bestScore = 0;
+    }
+ 
+    void Update() 
+	{
+        score = Mathf.RoundToInt(Mathf.Abs(transform.position.x - startPosition.x));
+        bestScore = Mathf.Max(score, bestScore);
+    }
+ 
+    public void OnGameOver() 
+	{
+        transform.position = startPosition;
+    }
+
+	void displayText() 
+	{
+		myText.text = "This is my sample text";
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
