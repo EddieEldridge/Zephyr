66da1768274fb123fc66af6abd8bc070b436bf70
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-04-04 19:48:23 +0100

    Adding checkpoint system and finished 2 levels

---
+++ Assets/Scripts/CheckpointController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointController : MonoBehaviour {
+
+    // Variables
+    public Sprite flag1;
+    public Sprite flag2;
+
+    private SpriteRenderer checkpointSpriteRenderer;
+    public bool checkpointReached;
+
+	// Use this for initialization
+	void Start ()
+    {
+        checkpointSpriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            checkpointSpriteRenderer.sprite = flag;
+            checkpointReached = true;
+        }
+    }
+}
