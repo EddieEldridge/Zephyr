a83f3bbff86a6dce6f9339f766a05e8a9906996d
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-04-10 23:09:02 +0100

    Adding highscore to timer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -24,7 +24,36 @@ public class Timer : MonoBehaviour {
 
         if (timerText != null)
         {
-            timerText.text = "TIME: " + timer.ToString(); // LINE WITH PROBLEM
+            timerText.text = "TIME: " + timer.ToString();
+
+            // Create a temporary reference to the current scene.
+            Scene currentScene = SceneManager.GetActiveScene();
+
+            // Retrieve the name of this scene.
+            string sceneName = currentScene.name;
+
+            if (sceneName == "Example 1")
+            {
+                // Do something...
+            }
+            else if (sceneName == "Example 2")
+            {
+                // Do something...
+            }
+
+            // Retrieve the index of the scene in the project's build settings.
+            int buildIndex = currentScene.buildIndex;
+
+            // Check the scene name as a conditional.
+            switch (buildIndex)
+            {
+                case 0:
+                    // Do something...
+                    break;
+                case 1:
+                    // Do something...
+                    break;
+            }
         }
     }
 }
