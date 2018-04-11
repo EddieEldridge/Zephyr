e7744420a31c3a004c23207d6491572e30c33ef2
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-04-11 14:06:22 +0100

    Added some soft lighting effects

--- Assets/FadeEffect.cs
+++ Assets/FadeEffect.cs
@@ -6,7 +6,7 @@ public class FadeEffect : MonoBehaviour {
 
     // Variables
     public Texture2D fadeOuteTexture; // the texture that will overlay
-    public float fadeSpeed = 0.3f; // speed of the fade effect
+    public float fadeSpeed = 0.1f; // speed of the fade effect
     private int drawDepth = -1000; // textures draw order
     private float alpha = 1.0f; // texture's alpha values (0,1)
     private int fadeDir = -1; // direction to fade, -1 = fade in, 1 = fade out
